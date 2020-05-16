13a259be3bd8101c8a783b0d6898871c36b980a8
Amy de Buitléir <amy@nualeargais.ie>
2020-05-17 00:18:38 +0100

    use hs-nix-template

--- default.nix
+++ default.nix
@@ -1,3 +1,47 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ compiler ? "ghc8101" }:
 
-pkgs.haskellPackages.callCabal2nix "som" ./. {}
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+
+  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = hself: hsuper: {
+      "som" =
+        hself.callCabal2nix
+          "som"
+          (gitignore ./.)
+          {};
+    };
+  };
+
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p."som"
+    ];
+    buildInputs = with pkgs.haskellPackages; [
+      myHaskellPackages.cabal-install
+      ghcid
+      ormolu
+      hlint
+      (import sources.niv {}).niv
+      pkgs.nixpkgs-fmt
+    ];
+    withHoogle = true;
+  };
+
+  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."som");
+
+  docker = pkgs.dockerTools.buildImage {
+    name = "som";
+    config.Cmd = [ "${exe}/bin/som" ];
+  };
+in
+{
+  inherit shell;
+  inherit exe;
+  inherit docker;
+  inherit myHaskellPackages;
+  "som" = myHaskellPackages."som";
+}
